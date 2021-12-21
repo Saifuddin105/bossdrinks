1405b523dfcd032cff4eebaf18b7f8ba310a6055
Arif Uddin <arifunctg@gmail.com>
2021-12-22 00:10:32 +0600

    https enforce

--- project/routes/web.php
+++ project/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-if (env('APP_ENV') === 'production') {
-    URL::forceSchema('https');
+if($this->app->environment('production')) {
+    URL::forceScheme('https');
 }
 
 // ************************************ ADMIN SECTION **********************************************
